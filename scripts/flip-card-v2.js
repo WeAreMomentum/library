9cbb4d878ca1887b6bc8df188dfdc79e5febdc4f
aknoebl <aleks@wearemomentum.at>
2024-01-15 14:25:46 +0100

    Added attribute "aria-pressed"

--- scripts/flip-card-v2.js
+++ scripts/flip-card-v2.js
@@ -6,20 +6,20 @@
   class Wrapper {
     constructor(htmlElement) {
       this.element = htmlElement;
-      this.element.setAttribute('aria-pressed', false);
+      this.element.ariaPressed = "false";
       this.showFront = this.showFront.bind(this);
       this.showBack = this.showBack.bind(this);
       this.element.addEventListener('click', this.showBack);
     }
     showFront() {
       this.element.style.transform = 'perspective(1000px) rotateY(0deg)';
-      this.element.setAttribute('aria-pressed', true);
+      this.element.ariaPressed = "true";
       this.element.removeEventListener('click', this.showFront);
       this.element.addEventListener('click', this.showBack);
     }
     showBack() {
       this.element.style.transform = 'perspective(1000px) rotateY(180deg)';
-      this.element.setAttribute('aria-pressed', false);
+      this.element.ariaPressed = "false";
       this.element.removeEventListener('click', this.showBack);
       this.element.addEventListener('click', this.showFront);
     }
