042a9be6b3ddc1c4a1ce8df3feeabfc4dec877cf
aknoebl <aleks@wearemomentum.at>
2024-04-22 14:35:46 +0200

    HCI saison dev

--- projects/hcinnsbruck/saison.js
+++ projects/hcinnsbruck/saison.js
@@ -226,29 +226,28 @@
     }
   };
 
-  /* Team */
-  const reqURL3 = "https://api.hockeydata.net/data/ih/GetTeamDetails" +
+  /* Spielerstatistiken */
+  const reqURL3 = "https://api.hockeydata.net/data/ih/LeaderFieldPlayers" +
     "?apiKey=738aba5a0c15ea7da496e1cda6922ff1" +
     "&referer=" + referer +
-    "&divisionId=" + divisionId +
-    "&teamId=" + teamId;
+    "&divisionId=" + divisionId;
   const xhr3 = new XMLHttpRequest();
   xhr3.open("GET", reqURL3);
   xhr3.send();
   xhr3.responseType = "json";
   xhr3.onload = () => {
     if (xhr3.readyState == 4 && xhr3.status == 200) {
-      const teamData = xhr3.response.data.teamData;
-      const playerStats = xhr3.response.data.playerStats;
+      const leaderboard = xhr3.response.data.rows;
+      const teamLeaderboard = leaderboard.filter(playerData => playerData.teamId == teamId);
       const teamTableDom = document.querySelector('.api__team_table');
       const tbodyDom = teamTableDom.querySelector('tbody');
       const trDom = tbodyDom.querySelector('tr');
-      playerStats.forEach(playerData => {
+      teamLeaderboard.forEach(playerData => {
         const newTrDom = trDom.cloneNode(true);
-        newTrDom.querySelector('.api__pos').textContent = '';
+        newTrDom.querySelector('.api__rank').textContent = leaderboard.indexOf(playerData) + 1;
         newTrDom.querySelector('.api__player_name').textContent = playerData.playerLastname + ' ' + playerData.playerFirstname;
-        newTrDom.querySelector('.api__team_logo img').src = teamData.images.teamLogo;
-        newTrDom.querySelector('.api__team_shortname').textContent = teamData.teamShortname;
+        //newTrDom.querySelector('.api__team_logo img').src = teamData.images.teamLogo;
+        newTrDom.querySelector('.api__team_shortname').textContent = playerData.teamShortname;
         newTrDom.querySelector('.api__player_jersey_nr').textContent = playerData.playerJerseyNr;
         newTrDom.querySelector('.api__position').textContent = playerData.position;
         newTrDom.querySelector('.api__games_played').textContent = playerData.gamesPlayed;
@@ -256,7 +255,7 @@
         newTrDom.querySelector('.api__assists').textContent = playerData.assists;
         newTrDom.querySelector('.api__points').textContent = playerData.points;
         newTrDom.querySelector('.api__plus_minus').textContent = playerData.plusMinus;
-        newTrDom.querySelector('.api__pim').textContent = '';
+        newTrDom.querySelector('.api__penalty_minutes').textContent = playerData.penaltyMinutes;
         tbodyDom.appendChild(newTrDom);
       });
       trDom.remove();
